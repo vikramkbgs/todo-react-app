1087447452ad65b2f64c3ff36619da0cb94a3111
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-02-09 09:31:18 +0530

    texting api

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,76 +1,108 @@
-import React, { useState } from "react";
-import TodoForm from "./TodoForm";
-import Todo from "./Todo";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // TodoList component
 function TodoList() {
   // State to store all the todos
   const [todos, setTodos] = useState([]);
 
-  // Function to add a new todo
-  const addTodo = (todo) => {
-    // Check if the todo text is not empty or only whitespace
-    if (!todo.text || /^\s*$/.test(todo.text)) {
-      return;
-    }
+  // State to store the new todo being added
+  const [newTodo, setNewTodo] = useState({ title: "", completed: false });
 
-    // Add the new todo to the front of the todos array
-    const newTodos = [todo, ...todos];
+  // Use useEffect hook to fetch the list of to-dos from a remote API when the component is mounted
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos?_limit=5"
+      );
+      setTodos(response.data);
+    };
 
-    // Update the todos state with the new todos array
-    setTodos(newTodos);
+    fetchTodos();
+  }, []);
+
+  // Handle changes to the input for adding a new todo
+  const handleNewTodoChange = (e) => {
+    setNewTodo({ ...newTodo, [e.target.name]: e.target.value });
   };
 
-  // Function to update an existing todo
-  const updateTodo = (todoId, newValue) => {
-    // Check if the updated todo text is not empty or only whitespace
-    if (!newValue.text || /^\s*$/.test(newValue.text)) {
-      return;
-    }
-
-    // Map over the previous todos state and return the updated todo
-    setTodos((prev) =>
-      prev.map((item) => (item.id === todoId ? newValue : item))
+  // Handle adding a new todo
+  const handleAddTodo = async (e) => {
+    e.preventDefault();
+    // Make a post request to the API to add the new todo
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      newTodo
     );
+
+    // Update the todos state with the newly added todo
+    setTodos([...todos, response.data]);
   };
 
-  // Function to remove a todo
-  const removeTodo = (id) => {
-    // Filter the todos array to exclude the todo with the given id
-    const removedArr = [...todos].filter((todo) => todo.id !== id);
+  // Handle updating a todo
+  const handleUpdateTodo = async (id, updatedTodo) => {
+    // Make a put request to the API to update the todo
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${id}`,
+      updatedTodo
+    );
 
-    // Update the todos state with the filtered todos array
-    setTodos(removedArr);
+    // Update the todos state with the updated todo
+    setTodos(todos.map((todo) => (todo.id === id ? response.data : todo)));
   };
 
-  // Function to toggle the completion status of a todo
-  const completeTodo = (id) => {
-    // Map over the todos array and toggle the completion status of the todo with the given id
-    let updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
-      }
-      return todo;
-    });
-    // Update the todos state with the updated todos array
-    setTodos(updatedTodos);
+  // Handle deleting a todo
+  const handleDeleteTodo = async (id) => {
+    // Make a delete request to the API to delete the todo
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+    // Update the todos state by removing the deleted todo
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
-    <>
-      {/* Title */}
-      <h1>What's the Plan for Today?</h1>
-      {/* TodoForm component with the addTodo function passed as a prop */}
-      <TodoForm onSubmit={addTodo} />
-      {/* Todo component with all the necessary functions and state passed as props */}
-      <Todo
-        todos={todos}
-        completeTodo={completeTodo}
-        removeTodo={removeTodo}
-        updateTodo={updateTodo}
-      />
-    </>
+    <div>
+      {/* Display the header */}
+      <h1>Todo List</h1>
+
+      {/* Form to add a new todo */}
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          name="title"
+          value={newTodo.title}
+          onChange={handleNewTodoChange}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      {/* Display the list of todos */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {/* Display the todo title */}
+            {todo.title}
+
+            {/* Button to mark the todo as complete/incomplete */}
+            <button
+              onClick={() =>
+                handleUpdateTodo(todo.id, {
+                  ...todo,
+                  completed: !todo.completed,
+                })
+              }
+            >
+              {todo.completed ? "Mark Incomplete" : "Mark Complete"}
+            </button>
+
+            {/* Button to delete the todo */}
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
+
 }
 
 export default TodoList;
